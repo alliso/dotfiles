cfedfcc5fc4328e4e2afe308b4a5261caaa5e18f
alliso <alejandro.lliso.cosin@gmail.com>
2024-06-09 19:12:04 +0200

    feat(wezterm): added wezterm config
    it is added using org-mode

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
+
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+
+config.enable_tab_bar = false
+
+config.font_size = 14
+
+return config
