e73290c65af81a988ae5aa2b256b3388ad1634c2
alliso <alejandro.lliso.cosin@gmail.com>
2024-06-19 23:37:51 +0200

    feat(zellij): added zellij config

--- zsh/custom/navigation.zsh
+++ zsh/custom/navigation.zsh
@@ -2,7 +2,7 @@ function cds(){
    dir=$(ls /Users/alliso/Documents/services/ | fzf)
    full_dir="/Users/alliso/Documents/services/$dir"
 
-   zellij options --session-name $dir --default-cwd $full_dir --default-layout /Users/alliso/Documents/dotfiles/zellij/layout.kdl
+   zellij options --session-name $dir --default-cwd $full_dir --default-layout /Users/alliso/Documents/dotfiles/zellij/services_layout.kdl
 }
 
 function cdr(){
