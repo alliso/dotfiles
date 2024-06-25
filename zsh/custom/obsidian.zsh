53f7d94149f54021f9a4de595072bb0560583d08
alliso <alejandro.lliso.cosin@gmail.com>
2024-06-25 15:49:12 +0200

    feat(zsh): changed home path to $HOME

--- zsh/custom/obsidian.zsh
+++ zsh/custom/obsidian.zsh
@@ -5,7 +5,7 @@ function on(){
     return
   fi
   filename=$1.md
-  tmux new-session -A "cd /Users/alliso/Documents/obsidian-vaults/ctt/inbox && touch $filename && nvim $filename"
+  tmux new-session -A "cd $HOME/Documents/obsidian-vaults/ctt/inbox && touch $filename && nvim $filename"
 }
 
 function oe(){
@@ -16,7 +16,7 @@ function oe(){
   fi
   filename=$1.md
   sname="obsidian-creator"
-  tmux new-session -D -s $sname -A "cd /Users/alliso/Documents/obsidian-vaults/ctt/inbox && touch $filename && nvim $filename"
+  tmux new-session -D -s $sname -A "cd $HOME/Documents/obsidian-vaults/ctt/inbox && touch $filename && nvim $filename"
   tmux send-keys -t $name ":ObsidianTemplate drawing_template"
   tmux kill-session -t $name
   open obsidian://vault/ctt/$filename
