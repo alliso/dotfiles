1c14867f64d88cef09fdc1de6d5f9d3c947b0d8d
alliso <alejandro.lliso.cosin@gmail.com>
2024-07-11 12:13:32 +0200

    feat(zsh): attach to running session if name matches

--- zsh/custom/navigation.zsh
+++ zsh/custom/navigation.zsh
@@ -11,12 +11,28 @@ function cdr(){
         dir=$(ls $HOME/Documents/CTT/ | fzf)
         full_dir="$HOME/Documents/CTT/$dir"
 
+        session=$(zellij ls | grep $dir | wc -l)
+        if [ $session -eq 1 ]
+        then
+            echo $session
+            zellij attach $dir
+            return
+        fi
+
         zellij options --session-name $dir --default-cwd $full_dir
         return
     fi
 
     dir=$(ls $HOME/Documents/CTT/ | fzf -q $1)
     full_dir="$HOME/Documents/CTT/$dir"
+
+    session=$(zellij ls | grep $dir | wc -l)
+    if [ $session -eq 1 ]
+    then
+        echo $session
+        zellij attach $dir
+        return
+    fi
 
     zellij options --session-name $dir --default-cwd $full_dir
 }
