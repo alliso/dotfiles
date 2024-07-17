6468fea6f5368893ba766b2aebddb89e2ed07cd8
alliso <alejandro.lliso.cosin@gmail.com>
2024-07-17 12:06:50 +0200

    feat(zsh): gdl and zjs

--- zsh/custom/zellij.zsh
+++ zsh/custom/zellij.zsh
@@ -4,3 +4,8 @@ function zja(){
          session=$(zellij ls -s | fzf)
          zellij a $session
 }
+
+function zjs(){
+         session_name=$(pwd | awk -F'/' '{print $NF}')
+         zj --session $session_name
+}
