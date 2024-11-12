2121ab9b467a3115e3b7e2d53a5913a4eea4f08b
alliso <alejandro.lliso.cosin@gmail.com>
2024-11-12 16:16:58 +0100

    feat(doom): added go lsp and fixed some folder links

--- zsh/custom/obsidian.zsh
+++ zsh/custom/obsidian.zsh
@@ -4,13 +4,13 @@ function ov(){
         dir=$(ls $HOME/Documents/obsidian-vaults/ | fzf)
         full_dir="$HOME/Documents/obsidian-vaults/$dir"
 
-        zellij options --session-name $dir --default-cwd $full_dir --default-layout $HOME/Documents/dotfiles/zellij/obsidian_layout.kdl
+        zellij options --session-name $dir --default-cwd $full_dir --default-layout $HOME/dotfiles/zellij/.config/zellij/obsidian_layout.kdl
         return
     fi
 
     dir=$(ls $HOME/Documents/obsidian-vaults/ | fzf -q $1)
     full_dir="$HOME/Documents/obsidian-vaults/$dir"
 
-    zellij options --session-name $dir --default-cwd $full_dir --default-layout $HOME/Documents/dotfiles/zellij/obsidian_layout.kdl
+    zellij options --session-name $dir --default-cwd $full_dir --default-layout $HOME/dotfiles/zellij/.config/zellij/obsidian_layout.kdl
 
 }
